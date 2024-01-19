f8fcdbc270d53f2bcec09d6b3dab4fa8a7cf6ecd
Elias Palmer <elias@toddsden.com>
2024-01-19 12:06:47 -0500

    Revert "Uknown Commit"
    This reverts commit 66af9fdd9f0c3638fbd93e14b8f42562fe19f67e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,57 @@
-import React from "react";
-import { Container, Switch, darkScrollbar } from "@mui/material";
-import { experimentalStyled as styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Nav from "./Nav";
-import Card from "./Card";
-import DarkSwitch from "./DarkSwitch";
+import React from 'react';
+import { Container, darkScrollbar } from '@mui/material';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Nav from "./Nav"
+import Card from "./Card"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  },
+});
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(2),
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
 function App() {
-  function toggleMode() {
-    return (prefersDarkMode = !prefersDarkMode);
-  }
+  const [darkMode, setDarkMode] = React.useState(false)
+  const [themeMode, setThemeMode] = React.useState("dark")
+    function toggleMode() {
+        setDarkMode(!darkMode)
+        console.log(darkMode)
+    }
 
   return (
-    <Container>
-      <Nav clicked={toggleMode} />
-      <Container sx={{ mt: 3 }}>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 1, sm: 8, md: 12 }}
-          >
-            {Array.from(Array(6)).map((_, index) => (
-              <Grid item xs={1} sm={4} md={4} key={index}>
-                <Card image={"https://source.unsplash.com/random"}></Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box display="flex" justifyContent="end">
-          <DarkSwitch />
-        </Box>
-      </Container>
-    </Container>
-  );
+    <>
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+        <Container>
+          <Nav clicked={toggleMode} />
+            <Container sx={{ mt: 3}}>
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
+                  {Array.from(Array(6)).map((_, index) => (
+                    <Grid item xs={1} sm={4} md={4} key={index}>
+                      <Card image={"https://source.unsplash.com/random"} ></Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+            </Container>
+        </Container>
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,16 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 export default function ButtonAppBar(props) {
+    
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -26,13 +27,7 @@ export default function ButtonAppBar(props) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Photo Grid Display
           </Typography>
-          <Button
-            onClick={props.clicked}
-            color="inherit"
-            endIcon={<Brightness4Icon />}
-          >
-            Mode
-          </Button>
+          <Button onClick={props.clicked} color="inherit" endIcon={<Brightness4Icon />}>Mode</Button>
         </Toolbar>
       </AppBar>
     </Box>
