a6b19dac3bd2b496d213dbe1f601b5d1935cfe74
Elias Palmer <elias@toddsden.com>
2024-02-04 17:44:05 -0500

    Got exifr to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,22 @@
-import React from 'react';
-import { Container, darkScrollbar } from '@mui/material';
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Nav from "./Nav"
-import Card from "./Card"
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import Nav from "./Nav";
+import Altitude from "./images/altitude.jpg";
+import exifr from "exifr";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark"
-  },
-});
+let image = Altitude;
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+let parsedImage = exifr.parse(Altitude, true);
 
-function App() {
-  const [darkMode, setDarkMode] = React.useState(false)
-  const [themeMode, setThemeMode] = React.useState("dark")
-    function toggleMode() {
-        setDarkMode(!darkMode)
-        console.log(darkMode)
-    }
-
+export default function App() {
   return (
-    <>
-      <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-        <Container>
-          <Nav clicked={toggleMode} />
-            <Container sx={{ mt: 3}}>
-              <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-                  {Array.from(Array(6)).map((_, index) => (
-                    <Grid item xs={1} sm={4} md={4} key={index}>
-                      <Card image={"https://source.unsplash.com/random"} ></Card>
-                    </Grid>
-                  ))}
-                </Grid>
-              </Box>
-            </Container>
-        </Container>
-      </ThemeProvider>
-    </>
-  )
+    <Box>
+      <Nav />
+      <Button variant="contained" onClick={() => console.log(parsedImage)}>
+        Console
+      </Button>
+      <img style={{ height: 400 }} src={Altitude}></img>
+      <Typography></Typography>
+    </Box>
+  );
 }
-
-export default App
